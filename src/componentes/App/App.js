2a6da889e6a75a122eaf0808c56a23437f3fb99b
maribilinski <160802147+maribilinski@users.noreply.github.com>
2024-07-06 02:09:18 -0400

    Update App.js

--- src/componentes/App/App.js
+++ src/componentes/App/App.js
@@ -1,59 +1,44 @@
-import { Component } from "react";
+import { useState } from "react";
 import Estudiante from "../Estudiante/Estudiante";
 
-class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      contador: 1,
-      estudiantes: [{
-        nombre: "Jane",
-        apellido: "Doe",
-        edad: 46,
-        hairColor: "negro"
-      },
-      {
-        nombre: "John",
-        apellido: "Smith",
-        edad: 80,
-        hairColor: "marron"
-      },
-      {
-        nombre: "Jess",
-        apellido: "Smith",
-        edad: 65,
-        hairColor: "negro"
-      },
-      {
-        nombre: "Maria",
-        apellido: "Millord",
-        edad: 46,
-        hairColor: "marron"
-      }
-    ]
-    }
-  }
+const App = () => {
+  const estudiantesInicial = [{
+    nombre: "Jane",
+    apellido: "Doe",
+    edad: 45,
+    hairColor: "Negro"
+  },
+  {
+    nombre: "John",
+    apellido: "Smith",
+    edad: 88,
+    hairColor: "Marron"
+  },
+  {
+    nombre: "Millard",
+    apellido: "Fillmore",
+    edad: 50,
+    hairColor: "Negro"
+  },
+  {
+    nombre: "Maria",
+    apellido: "Smith",
+    edad: 62,
+    hairColor: "Marron"
+  }];
 
-  incrementarEdad = () =>{
-    this.setState({
-      contador: this.state.contador +1
-    });
-  }
-
-  render = () => {
-    return(
-      <>
-        <h1>Contador es {this.state.contador}</h1>
-        {this.state.estudiantes.map((estudiante) => {
-          return (<Estudiante nombre={estudiante.nombre}
-                              apellido={estudiante.apellido}
-                              edad={estudiante.edad}
-                              hairColor={estudiante.hairColor}
-                              incrementarEdad = {this.incrementarEdad}/>)
-        })}
-      </>
-    )
-  }
+  const [listaEstudiantes, setListaEstudiantes] = useState(estudiantesInicial);
+  
+  return (
+    <>
+      listaEstudiantes.map ((estudiante) => {
+        return (<Estudiante nombre={estudiante.nombre}
+                            apellido= {estudiante.apellido}
+                            edad = {estudiante.edad}
+                            hairColor = {estudiante.hairColor}/>)
+      })
+    </>
+  );
 }
 
 export default App;
